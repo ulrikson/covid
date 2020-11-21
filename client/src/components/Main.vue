8440b552e22276761c6780f42ee411c40083134e
Erik Ulrikson <erik.ulrikson@gmail.com>
2020-11-21 16:11:17 +0100

    setting up api call for chart data

--- client/src/components/Main.vue
+++ client/src/components/Main.vue
@@ -1,13 +1,35 @@
 <template>
 	<div>
 		<h1 class="text-6xl text-gray-400">HEJ</h1>
+		<div>{{timeline}}</div>
 	</div>
 </template>
 
 <script>
 
+import axios from 'axios';
+
 export default {
-	name: 'HelloWorld',
+	name: 'Main',
+
+	data() {
+		return {
+			timeline: ''
+		}
+	},
+
+	mounted() {
+		this.getTimeline();
+	},
+
+	methods: {
+		getTimeline() {
+			axios.get('http://localhost:5000/timeline')
+            .then((res) => {
+                this.timeline = res.data;
+            });
+		}
+	}
 }
 
 </script>
--- server/app.py
+++ server/app.py
@@ -1,14 +1,22 @@
 from flask import Flask, render_template, request
 from flask_cors import CORS, cross_origin
+from api import timeline
 
 # instantiate the app
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+@app.route('/timeline', methods=['GET'])
+def getTimeline():
+    return timeline()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
