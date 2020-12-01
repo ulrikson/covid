4009cae9fda488c2c264a740af81e141b0b395cf
Erik Ulrikson <ullebulle@eriks-mbp.mynet>
2020-12-01 16:23:05 +0100

    latest stats frontend

--- client/src/components/Main.vue
+++ client/src/components/Main.vue
@@ -5,13 +5,19 @@
 				<h1 class="font-bold text-xl text-white">COVID I SVERIGE</h1>
 				<a href="javascript:void(0);" @click.prevent="refreshData"><refresh-icon :class="['ml-2 h-5 w-5 text-white', {'animate-spin': loading}]"/></a>
 			</div>
+
+			<p class="text-gray-400 text-xs mt-4">Totalt:</p>
 			<div class="flex flex-wrap">
-				<!-- for-loop? -->
-				<div class="flex items-center mt-4 bg-semiDark rounded-3xl px-3 py-2 text-sm text-gray-400">
-					<confirmed-icon class="mr-2 h-5 w-5 text-red-200"/>250.000 bekräftade
+				<div v-for="(stat, index, i) in latestStats" :key="index" :class="['flex items-center mt-2 bg-semiDark rounded-3xl px-3 py-2 text-sm text-gray-400', {'md:ml-2': i >= 1}]">
+					<span class="flex">
+						<confirmed-icon v-if="index == 'confirmed'" class="mr-2 h-5 w-5 text-red-200"/>
+						<dead-icon v-else class="mr-2 h-5 w-5 text-red-600"/>
+						{{formatNumber(stat)}} {{index == 'confirmed' ? 'bekräftade' : 'döda'}}
+					</span>
 				</div>
 			</div>
-			<div class="mt-4">
+
+			<div class="mt-2">
 				<div class="flex justify-end items-center">
 					<label class="text-gray-400 mr-2 text-xs">Experimentell</label>
 					<input type="checkbox" v-model="experimental" class="h-2.5 w-2.5"/>
@@ -31,9 +37,11 @@
 					</a>
 				</div>
 			</div>
+
 			<div class="flex w-full justify-end items-center min-h-8">
 				<p v-if="choices.period == 'linear'" class="text-gray-400">{{r2Percentage}}</p>
 			</div>
+
 			<div class="w-full bg-semiDark flex justify-center rounded-3xl py-10">
 				<chart ref="lineChart" @extra-info="handleExtraInfo"/>
 			</div>
@@ -47,6 +55,7 @@ import axios from 'axios';
 
 import RefreshIcon from './icons/Refresh.vue';
 import ConfirmedIcon from './icons/Confirmed.vue';
+import DeadIcon from './icons/Dead.vue';
 
 import Chart from './Chart.vue';
 import StatButton from './StatButton.vue';
@@ -58,6 +67,7 @@ export default {
 		Chart,
 		RefreshIcon,
 		ConfirmedIcon,
+		DeadIcon,
 		StatButton
 	},
 
@@ -167,6 +177,10 @@ export default {
 		getLinear() {
 			this.$refs.lineChart.getLinear(this.choices);
 			this.choices.period = 'linear';
+		},
+
+		formatNumber(number) {
+			return number.toLocaleString('sv');
 		}
 	}
 }
